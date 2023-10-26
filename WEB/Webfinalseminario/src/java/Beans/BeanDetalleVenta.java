62574f9f765d93e467f3949938c0bf27c1d6c1bb
DiegoSamayoaGomez <samayoasg2014@gmail.com>
2023-10-25 23:51:58 -0600

    resolución de conflictos de nombres

--- WEB/Webfinalseminario/src/java/Beans/BeanDetalleVenta.java
+++ WEB/Webfinalseminario/src/java/Beans/BeanDetalleVenta.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Bean;
+package Beans;
 import POJOs.Producto;
 import POJOs.Venta;
 import java.math.BigDecimal;
